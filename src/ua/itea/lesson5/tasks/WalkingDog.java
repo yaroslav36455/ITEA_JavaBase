e53239d294e4cca36cd365301c98342a67fe4bd9
Yaroslav Tarasenko <yaroslav36455@gmail.com>
2020-08-27 01:38:15 +0300

    Add bomb and medicine chest

--- src/ua/itea/lesson5/tasks/WalkingDog.java
+++ src/ua/itea/lesson5/tasks/WalkingDog.java
@@ -11,21 +11,24 @@ public class WalkingDog {
 		/* Game state */
 		boolean isGameOver = false;
 		boolean isQuit = false;
-		int dogPos = 0;
 		int health = 100;
-		int stepPayment = -5;
+		int dogPos = 0;
+		int bombPos = (int) (Math.random() * 9) + 1;
+		int aidPos  = bombPos;
+		while (aidPos == bombPos) {
+			aidPos = (int) (Math.random() * 9) + 1;
+		}
+		
+		/* Step constants */
+		int STEP_PAYMENT = -5;
+		int STEP_ON_BOMB_PAYMENT = -40;
+		int STEP_ON_AID_PAYMENT  = 40;
 		
 		/* Buttons */
 		char buttonPressed;
 		char leftButton  = 'a';
 		char rightButton = 'd';
 		char quitButton  = 'q';
-		
-		/* Game field */
-		char[] field = new char[10];
-		char floor = '_';
-		char dog   = '@';
-		Arrays.fill(field, floor);
 		
 		/* Print info */
 		System.out.println("Control:");
@@ -34,17 +37,27 @@ public class WalkingDog {
 		System.out.println(" quit  - '" + quitButton  + "'.");
 		System.out.println("-------------");
 		
+		/* Game field */
+		char[] field = new char[10];
+		char floor = '_';
+		char dog   = '@';
+		char bomb  = '*';
+		char aid   = '+';
+		Arrays.fill(field, floor);
+		field[bombPos] = bomb;
+		field[aidPos]  = aid;
+		
 		while(!isQuit) {
 			
-			/* Update */
-			field[dogPos] = dog;
+			/* Update position */
+			field[dogPos]  = dog;
 			
 			/* Draw */
 			System.out.print("[");
 			for (int i = 0; i < field.length; i++) {
 				System.out.print(field[i]);
 			}
-			System.out.print("] | health: " + health);
+			System.out.printf("] | health: %3d", health);
 			
 			/* Request pressing */
 			System.out.print(" | press: ");
@@ -55,25 +68,34 @@ public class WalkingDog {
 				isQuit = true;
 			} else if (buttonPressed == leftButton) {
 				field[dogPos--] = floor;
-				health += stepPayment;
 			} else if (buttonPressed == rightButton) {
 				field[dogPos++] = floor;
-				health += stepPayment;
+			}
+			
+			/* Health update */
+			health += STEP_PAYMENT;
+			if (dogPos == bombPos) {
+				health += STEP_ON_BOMB_PAYMENT;
+				bombPos = -1;
+			} else if (dogPos == aidPos) {
+				health += STEP_ON_AID_PAYMENT;
+				aidPos = -1;
 			}
 			
 			/* is Game Over */
 			if ((dogPos < 0 || dogPos >= field.length) || health < 1) {
 				isGameOver = true;
 				isQuit = true;
-			}	
+			}
 		}
 		
-		scanner.close();
 		if (isGameOver) {
 			System.out.println("Health: " + health);
 			System.out.println("Game Over");
 		}
 		System.out.println("Quit");
+		
+		scanner.close();
 	}
 
 }
