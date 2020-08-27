c9a1062ec476cf68d7d6449d8887a6854656cde6
Yaroslav Tarasenko <yaroslav36455@gmail.com>
2020-08-27 14:51:42 +0300

    Change the type for money handling and rounding of money

--- src/ua/itea/lesson3/tasks/TaskFirst.java
+++ src/ua/itea/lesson3/tasks/TaskFirst.java
@@ -14,22 +14,22 @@ public class TaskFirst {
 	public static void main(String[] args) {
 		
 		/* Negative money is not allowed */
-		float money = 455.85f;
-		float discount;
+		double money = 455.85;
+		double discount;
   
 		/* Sort by money */
 		if (money >= 500) {
-			discount = 0.07f;
+			discount = 0.07;
 		} else if (money >= 400) {
-			discount = 0.05f;
+			discount = 0.05;
 		} else if (money >= 300) {
-			discount = 0.03f;
+			discount = 0.03;
 		} else {
-			discount = 0.0f;
+			discount = 0.0;
 		}
 		
-		System.out.println("Ваша скидка " + discount + "%, "
-				           + "и того к оплате " + money * (1. - discount)
-				           + " с учётом скидки.");
+		System.out.printf("Ваша скидка %.2f%%, "
+				           + "и того к оплате %.2f с учётом скидки.",
+				           discount, money * (1. - discount));
 	}
 }
