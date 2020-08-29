ad9b03a887041b62d7f8ed9cecddc7b900eb2952
Yaroslav Tarasenko <yaroslav36455@gmail.com>
2020-08-29 23:40:43 +0300

    Add Task3

---
+++ src/ua/itea/lesson6/tasks/Task3.java
@@ -0,0 +1,120 @@
+package ua.itea.lesson6.tasks;
+
+import java.util.Scanner;
+
+public class Task3 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String LETTERS = "-'абвгдеёжзийклмнопрстуфхцчшщъыьэюяabcdefghijklmnopqrstuvwxyz"; 
+		String CONSONANTS = "бвгджйзклмнпрстфхцчшщbcdfghjklmnpqrstvwxyz";
+		String firstLine;
+		String secondLine;
+		String firstLineLowerCase;
+		String secondLineLowerCase;
+		
+		int firstLineLength  = 0;
+		int secondLineLength = 0;
+		
+		int firstLineWordCount  = 0;
+		int secondLineWordCount = 0;
+		
+		int firstLineConsonantsCount  = 0;
+		int secondLineConsonantsCount = 0;
+		
+		
+		System.out.println("Enter first line:");
+		firstLine = scanner.nextLine();
+		
+		System.out.println("Enter second line:");
+		secondLine = scanner.nextLine();
+		
+		scanner.close();
+		
+		/* Print input */
+		System.out.println("---------------------------------");
+		System.out.println("First line : \"" + firstLine + "\"");
+		System.out.println("Second line: \"" + secondLine + "\"");
+		
+		
+		/* Create lower case strings*/
+		firstLineLowerCase  = firstLine.toLowerCase();
+		secondLineLowerCase = secondLine.toLowerCase();
+		
+		/* First task */
+		firstLineLength  = firstLine.length();
+		secondLineLength = secondLine.length();
+		
+		/* Second task: word count */
+		boolean insideWord = false;
+		for (int i = 0; i < firstLineLowerCase.length(); i++) {
+			boolean isLetter = false;
+			
+			/* Find char in LETTERS */
+			for (int j = 0; j < LETTERS.length(); j++) {
+				if (firstLineLowerCase.charAt(i) == LETTERS.charAt(j)) {
+					isLetter = true;
+					break;
+				}
+			}
+			
+			if (isLetter && !insideWord) {
+				insideWord = true;
+				firstLineWordCount++;
+			} else if (!isLetter) {
+				insideWord = false;
+			}
+		}
+		
+		insideWord = false;
+		for (int i = 0; i < secondLineLowerCase.length(); i++) {
+			boolean isLetter = false;
+			
+			/* Find char in LETTERS */
+			for (int j = 0; j < LETTERS.length(); j++) {
+				if (secondLineLowerCase.charAt(i) == LETTERS.charAt(j)) {
+					isLetter = true;
+					break;
+				}
+			}
+			
+			if (isLetter && !insideWord) {
+				insideWord = true;
+				secondLineWordCount++;
+			} else if (!isLetter) {
+				insideWord = false;
+			}
+		}
+		
+		
+		/* Third task: consonants count */
+		for (int i = 0; i < firstLineLowerCase.length(); i++) {
+			for (int j = 0; j < CONSONANTS.length(); j++) {
+				if (firstLineLowerCase.charAt(i) == CONSONANTS.charAt(j)) {
+					firstLineConsonantsCount++;
+				}
+			}
+		}
+		
+		for (int i = 0; i < secondLineLowerCase.length(); i++) {
+			for (int j = 0; j < CONSONANTS.length(); j++) {
+				if (secondLineLowerCase.charAt(i) == CONSONANTS.charAt(j)) {
+					secondLineConsonantsCount++;
+				}
+			}
+		}
+		
+		/* Output */
+		System.out.println("---------------------------------");
+		
+		System.out.println("Length of the first line : " + firstLineLength);
+		System.out.println("Length of the second line: " + secondLineLength);
+		
+		System.out.println("Words in the first line : " + firstLineWordCount);
+		System.out.println("Words in the second line: " + secondLineWordCount);
+		
+		System.out.println("Consonants in the first line : " + firstLineConsonantsCount);
+		System.out.println("Consonants in the second line: " + secondLineConsonantsCount);
+	}
+
+}
