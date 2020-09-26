a2fb837df2d52d7772f730308f8ef0e3257b2837
Yaroslav Tarasenko <yaroslav36455@gmail.com>
2020-09-26 14:52:07 +0300

    Add lesson14

---
+++ src/ua/itea/lesson14/Main.java
@@ -0,0 +1,23 @@
+package ua.itea.lesson14;
+
+//import java.sql.Connection;
+//import java.sql.DriverManager;
+//import java.sql.SQLException;
+
+// Notice, do not import com.mysql.jdbc.*
+// or you will have problems!
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            // The newInstance() call is a work around for some
+            // broken Java implementations
+
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            System.out.println("Driver works.");
+        } catch (Exception ex) {
+            // handle the error
+        	ex.printStackTrace();
+        }
+    }
+}
